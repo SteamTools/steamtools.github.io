b37b38f0e1916acd7900b61291601249d3a61624
Ehsan Kia <contact@ehsankia.com>
2016-01-23 15:42:12 -0500

    Fix name length filter in emoticons

--- backgrounds/main.js
+++ backgrounds/main.js
@@ -14,9 +14,6 @@ angular
 		} else if (limits.type === "Date") {
 			newMin = limits.min / 100 * limits.dateRange;
 			newMax = limits.max / 100 * limits.dateRange;
-		} else if (limts.type === "Length") {
-			newMin = 1;
-			newMax = 36;
 		}
 
 		var data = 0;
@@ -33,8 +30,6 @@ angular
 				data = input[i].price;
 			} else if (limits.type === "Date") {
 				data = input[i].time;
-			} else if (limits.type === "Length") {
-				data = input[i].name.length;
 			}
 
 			if (newMin <= data && data <= newMax)
